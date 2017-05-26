9d21a9f8da83a568cba2a83558d4e3f950c75ab5
DA\nwuser <daiki091548@gmail.com>
2017-05-26 10:46:17 +0900

    Revert "kaiteiban"
    This reverts commit 8851c8ae71bb872bec3d29849deb985e387e9773.

---
+++ yokoari/Assets/script/Discrimination.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Discrimination : MonoBehaviour {
+
+
+    enum Item
+    {
+         item1,
+         item2,
+         item3,
+         item4,
+         item5,
+    };
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ yokoari/Assets/script/Press_Space.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Press_Space : MonoBehaviour
+{
+    //[SerializeField]
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnPressDecide();
+        }
+    }
+
+    void OnPressDecide()
+    {
+
+        Debug.Log("avcfvcdxsa");
+        SceneManager.LoadScene("test");
+
+        
+    }
+}
