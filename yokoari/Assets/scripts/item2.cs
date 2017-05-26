8851c8ae71bb872bec3d29849deb985e387e9773
DA\nwuser <daiki091548@gmail.com>
2017-05-26 10:45:48 +0900

    kaiteiban

---
+++ yokoari/Assets/scripts/item2.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+public class item2 : MonoBehaviour
+{
+    public GameObject[] Train;
+
+    public int number;
+
+    void Start()
+    {
+        number = Random.Range(0, Train.Length);
+        Instantiate(Train[number], transform.position, transform.rotation);
+    }
+}
---
+++ yokoari/Assets/scripts/taitle.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class taitle : MonoBehaviour {
+
+    public float m_heigth_max;
+    public float m_heigth_min;
+
+
+    public Player_item player_item;
+    public int ii;
+    public int aa;
+    public int point;
+
+    public item2 item2;
+
+    public bool score_flg = false;
+
+
+        void Start()
+    {
+        ii = 0;
+        aa = 0;
+    }
+
+    void Update()
+    {
+        trans();
+
+        //aa = item2.number;
+
+        string id = player_item.item;
+        ii = PlayerPrefs.GetInt(id);
+        
+        /*
+        if(ii == aa)
+        {
+            FindObjectOfType<SCORE>().AddPoint(point);
+        }
+        */
+
+        Debug.Log("ii : " + ii);
+    }
+    
+
+    void trans()
+    {
+        Vector2 m_playerPos = GameObject.Find("player").transform.position;
+
+        //Debug.Log(m_playerPos);
+
+        if (m_heigth_min > m_playerPos.y)
+        {
+
+            SceneManager.LoadScene("yokeru");
+
+        }
+    }
+}
