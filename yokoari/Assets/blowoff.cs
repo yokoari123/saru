8832e1f238724dc25dbe116347a665e4a0d08708
DA\nwuser <apoapoapo1023@gmail.com>
2017-05-29 10:44:25 +0900

    吹き出しスクリプト 中に吹き出しを出す際の処理は入れていない、条件と変数次第で作業可能 わーい！たーのしー！

---
+++ yokoari/Assets/blowoff.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blowoff : MonoBehaviour {
+
+    public static int i=1;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (i != 10)
+        {
+            gameObject.transform.localScale += new Vector3(0.05f,0.05f, 1);
+            i++;
+        }
+	}
+}
